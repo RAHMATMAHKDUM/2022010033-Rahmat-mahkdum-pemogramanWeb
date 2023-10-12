1b8495febffab671bca7ccc2d69af2bd42ce753a
RAHMATMAHKDUM <rahmatmakdum@gmail.com>
2023-10-12 10:01:29 +0700

    first commit

---
+++ tugas2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>politeknik balekambang</title>
+  </head>
+  <body>
+    <main>
+      <h1>SELAMAT DATANG DI WEBSITE RPL POLIBANG JEPARA</h1>
+      <h2>Ayo kita belajar Web Devoloper bersama</h2>
+      <p>Rahmat Mahkdum</p>
+      <p>Pakis aji jepara DK Pungguk Indah</p>
+      <a href="https://www.polibang.ac.id/">klik disini untuk melihat Kampus polibang</a><br>
+      <a href="https://jagongoding.com/web/php/dasar/overview/">klik disini untuk bljr pemograman</a>
+    </main>
+  </body>
+</html>
