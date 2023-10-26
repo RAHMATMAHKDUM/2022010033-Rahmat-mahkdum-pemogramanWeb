f6ec1408ec05910ef3e5f11e4195ce5724b66d53
RAHMATMAHKDUM <rahmatmakdum@gmail.com>
2023-10-26 11:01:17 +0700

    Tugas

---
+++ Modul 2/Prak3_Rahmat_mahkdum.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <h1 align="center" >POLITEKNIK BALEKAMBANG</h1>
+        <h2 align="center" >NAMA MAHASISWA</h2>
+        <h3 align="center" >NAMA : RAHMAT MAHKDUM</h2>
+        <h4 align="center" >NIM  : 2022010033</h2>
+        <h1 align="center" >MATA KULIAH : PRAKTEK PERMOGRAMAN WEB 1</h1>
+    </p>
+</body>
+</html>
---
+++ Modul 5/sidebar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <frameset COLS="30%,70%">
+        <FRAME src="namaMH.html">
+        <div class="dropdown-content"></div>
+        <frame name="listname" src="rahmatbiodata.htm">
+        </FRAME>
+    </frameset>
+</body>
+</html>
